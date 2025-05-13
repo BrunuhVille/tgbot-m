80284ab1e89280a595beb6af4ff4e0a25b50f12b
BrunuhVille <55394446+BrunuhVille@users.noreply.github.com>
2025-05-13 15:58:44 +0800

    Update ydx_model.py

--- app/scripts/zhuque/ydx_model.py
+++ app/scripts/zhuque/ydx_model.py
@@ -558,7 +558,7 @@ async def zhuque_ydx_bet(client: Client, message: Message):
                                 return
 
 
-                remaining_bouns22 = 2750000
+                remaining_bouns22 = 3000000
                 
                 bet_counts22 = []
                 
